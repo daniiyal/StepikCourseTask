9c831fe49e5427dbf762b132ade1b364cdd86a19
daniiyal <danik.magomedow@mail.ru>
2020-06-18 13:50:52 +0300

    function name fixec

--- test_items.py
+++ test_items.py
@@ -2,7 +2,7 @@ import pytest
 import time
 from selenium.common.exceptions import NoSuchElementException
 
-def test_item(browser):
+def test_add_to_cart_button_is_displayed(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
     button = browser.find_element_by_css_selector(".btn.btn-lg.btn-primary.btn-add-to-basket")
